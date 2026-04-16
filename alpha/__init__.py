a39ca3d0f1be1ebc650e73b772c1ed7d107269b5
AKSHATDALAL842 <akshatdalal842@gmail.com>
2026-04-17 00:23:49 +0530

    feat: add MomentumAlpha BTC price momentum signal generator

--- alpha/__init__.py
+++ alpha/__init__.py
@@ -1,5 +1,6 @@
 # alpha/__init__.py
 from alpha.signal import AlphaSignal, AggregatedSignal
 from alpha.news_alpha import NewsAlpha
+from alpha.momentum_alpha import MomentumAlpha
 
-__all__ = ["AlphaSignal", "AggregatedSignal", "NewsAlpha"]
+__all__ = ["AlphaSignal", "AggregatedSignal", "NewsAlpha", "MomentumAlpha"]
